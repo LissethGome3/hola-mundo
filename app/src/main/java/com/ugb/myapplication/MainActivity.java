012bd055540358a0f66fcbb0e1d1df6fdfc3054a
https://github.com/LissethGome3/progra-ll.git <gameslisa0@gmail.com>
2021-02-19 07:39:18 -0600

    la calculadora

---
+++ app/src/main/java/com/ugb/myapplication/MainActivity.java
@@ -0,0 +1,9 @@
+package com.ugb.myapplication;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void calcular(View view) {
+    }
+}
